c6040dd8d0295d7a88183989b43b7fd9c996df2d
Shahid Ghafoor <shahidghafoor@macbook.local>
2019-05-08 15:08:17 +0500

    first commit

---
+++ app/src/main/java/com/byteshaft/solidariedadediria/sidebar_fragments/Movements.java
@@ -0,0 +1,27 @@
+package com.byteshaft.solidariedadediria.sidebar_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.byteshaft.solidariedadediria.R;
+
+public class Movements extends Fragment {
+
+
+    private View mBaseView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.movements));
+        mBaseView = inflater.inflate(R.layout.fragment_movements, container, false);
+        return mBaseView;
+    }
+}
